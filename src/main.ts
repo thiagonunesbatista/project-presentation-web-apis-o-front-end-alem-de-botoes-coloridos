0531c53782dc3323b84287ff87d29849404273e5
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2024-11-02 15:31:15 -0300

    build: install Vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// src/main.js
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Vuetify
+import "vuetify/styles"; // Import global styles
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import "./style.css";
+
+const vuetify = createVuetify({
+  components,
+  directives
+});
+
+const app = createApp(App);
+app.use(vuetify);
+app.mount("#app");
